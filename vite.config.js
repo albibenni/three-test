e71e6bccde88f6d4893f0189adc1746672178331
Alberto Benatti <albi.benni8@gmail.com>
2023-11-27 15:22:28 +0100

    fix glsl plugin vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 // vite.config.js
-// import glsl from "vite-plugin-glsl";
+import glsl from "vite-plugin-glsl";
 import { defineConfig } from "vite";
 import _config from "./_config.js";
 
@@ -7,7 +7,7 @@ const HOST = _config.server.host;
 const PORT = _config.server.port;
 
 export default defineConfig({
-  //   plugins: [glsl()],
+  plugins: [glsl()],
   server: {
     host: HOST,
     port: PORT,
